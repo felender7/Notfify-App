6a69c384200900f61f920e1a35bd3e3cd7a0ada2
Felender <felender7@gmail.com>
2021-08-10 19:31:49 +0200

    added new item menu

--- app/src/main/java/co/za/theappbrewery/notify/activities/community_alerts/CreateActivity.kt
+++ app/src/main/java/co/za/theappbrewery/notify/activities/community_alerts/CreateActivity.kt
@@ -8,5 +8,22 @@ class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+        setTheme(R.style.Theme_Notify)
+
+        //actionbar
+        val actionbar = supportActionBar
+
+        //set actionbar title
+        "Create Alert".also { actionbar!!.title = it }
+
+        //set back button
+        actionbar?.setDisplayHomeAsUpEnabled(true)
+        actionbar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    //back button function
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }

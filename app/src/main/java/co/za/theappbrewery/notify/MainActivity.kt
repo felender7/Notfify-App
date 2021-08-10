196438780c36b3b5ae308625c5c89952e467f4c3
Felender <felender7@gmail.com>
2021-08-10 16:50:31 +0200

    added button navbar

--- app/src/main/java/co/za/theappbrewery/notify/MainActivity.kt
+++ app/src/main/java/co/za/theappbrewery/notify/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.GridLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_Notify)
         setContentView(R.layout.activity_main)
+
+
+
+        //actionbar
+        val actionbar = supportActionBar
+
+        //set actionbar title
+        "DashBoard".also { actionbar!!.title = it }
+
+        //bottom Navigation
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
 
     }
 

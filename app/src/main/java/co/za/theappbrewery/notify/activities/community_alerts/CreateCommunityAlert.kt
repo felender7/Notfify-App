b55b1be060aaeab6477c35d1d677fdc801b0bf72
Felender <felender7@gmail.com>
2021-08-10 16:54:52 +0200

    created community alert activity

---
+++ app/src/main/java/co/za/theappbrewery/notify/activities/community_alerts/CreateCommunityAlert.kt
@@ -0,0 +1,11 @@
+package co.za.theappbrewery.notify.activities.community_alerts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateCommunityAlert : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_community_alert)
+    }
+}
